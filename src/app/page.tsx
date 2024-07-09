eb4dfcf671b5b9043dc3535a4066107fd3b3775b
hashmi mohammed <hashmi.smf@gmail.com>
2024-07-09 20:00:07 +0530

    added a sample api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("http://localhost:3002/api/get");
+  const data = await res.json();
+
   return (
     <div className="flex justify-between"> 
       <h1>welcome to nextjs practo</h1>
       <Link href="/dashboard">go to dashboard</Link>
-      
+      {JSON.stringify(data)}
     </div>
   );
 }
